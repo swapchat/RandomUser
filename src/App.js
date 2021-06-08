34efca39506f1137d911015e238d267f2ed1e0af
Swapnil Chaturvedi <swapnilchaturvedi96@gmail.com>
2021-06-08 12:12:06 +0530

    Add the random user project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import UsersList from "./components/UsersList";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          `https://randomuser.me/api/?page=${page}&results=10`
+        );
+        setUsers((users) => [...users, ...response.data.results]);
+        setErrorMsg("");
+      } catch (error) {
+        setErrorMsg("Error while loading data. Try again later.", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadUsers();
+  }, [page]);
+
+  const loadMore = () => {
+    setPage((page) => page + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+      <Header />
+      {errorMsg && <p className="errorMsg">{errorMsg}</p>}
+      <div className="load-more">
+        <button onClick={loadMore} className="btn-grad">
+          {isLoading ? "Loading..." : "Load More"}
+        </button>
+      </div>
+      <UsersList users={users} />
     </div>
   );
 }
